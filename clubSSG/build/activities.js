e00b5b099c6a44eb755992eb8d56624b9fe27dca
Adam Boyd <aboyd08@gmail.com>
2021-03-22 19:28:48 -0700

    Question 1 Done

--- clubSSG/build/activities.js
+++ clubSSG/build/activities.js
@@ -1,3 +1,28 @@
+let eventsTitles = ["Event Title", "Location and Date", "Time"];
+let pastEventsTitles = ["Event Title", "Location", "Date"];
+let nonClubTitles = ["Link", "Event Title", "Location", "Date"];
+
+let events = [{ "title": "&#x1F62D No Upcoming Events &#x1F62D", "location": "No Location", "date": "No Date", "time": "No Time Given" }];
+
+let pastEvents = [{ "title": "No Past Events Yet...", "location": "No Location", "date": "No Date" }];
+
+let nonClubEvents = [{
+    "imageLink": "<a href=\"https://www.intergem.com/events/upcoming-shows/san-mateo-ca-july-9-11-2021\"><img class=\"tiny center\" src=\"images/IntergemShow.png\" alt=\"Intergem Show Logo\"></a>",
+    "title": "Intergem Show",
+    "location": "San Mateo, CA",
+    "date": "July 9-11, 2021"
+}, {
+    "imageLink": "<a href=\"https://www.rockandmineralshows.com/Search/ListingDetails/vallejo-gem-mineral-society/384/false/false\"><img class=\"tiny center\" src=\"images/RockandMineralShows.jpg\" alt=\"Rock and Mineral Shows Logo\"></a>",
+    "title": "Vallejo Gem & Mineral Society",
+    "location": "Vallejo , CA",
+    "date": "February 27-28, 2021"
+}, {
+    "imageLink": "<a href=\"https://www.tradeindia.com/tradeshows/92078/the-fall-contra-costa-crystal-fair-2021.html\"><img class=\"tiny center\" src=\"images/CrystalFair.jpg\"alt=\"Crystal Fair Logo\"></a>",
+    "title": "The Fall Contra Costa Crystal Fair",
+    "location": "Walnut Creek, CA",
+    "date": "April 17-18, 2021"
+}];
+
 function createEvents() {
     let table = document.getElementById("events");
     events.forEach(function(element) {
@@ -17,7 +42,7 @@ function createPast() {
 }
 
 function createNonClub() {
-    var table = document.getElementById("nonClub");
+    let table = document.getElementById("nonClub");
     nonClubEvents.forEach(function(element) {
         let tr = document.createElement("tr");
         tr.innerHTML = `<td>${element.imageLink}</td><td>${element.title}</td><td>${element.location}</td><td>${element.date}</td>`;
--- clubSSG/processIt.js
+++ clubSSG/processIt.js
@@ -1,55 +1,3 @@
-# Homework #6 Solution
-
-**Adam Boyd**
-
-**NetID: xv3543**
-
-## Question 1
-
-### (a)
-
-### (b)
-
-### (c)
-
-**clubSSG/build/*.html**
-
-### (d)
-
-### (e)
-
-```HTML
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>{{title}}</title>
-        <meta name="author" content="{{author}}">
-        <meta name="description" content="{{description}}">
-        <link href="club.css" rel="stylesheet">
-    </head>
-
-<body>
-    <nav>
-        {% include "navMenu.html" %}
-    </nav>
-    <header>
-        <h1>{{header}}</h1>
-    </header>
-    <main>
-        {{mainContent | safe}}
-    </main>
-    <footer>
-        &copy; 2021 Adam Boyd
-    </footer>
-    <script src="{{scriptFile}}"></script>
-</body>
-</html>
-```
-
-### (f)
-
-```javascript
 const fs = require('fs');
 const nunjucks = require('nunjucks');
 const matter = require('gray-matter');
@@ -82,11 +30,3 @@ allFiles.forEach(function(srcName) {
     fs.writeFileSync(outName, outString);
     console.log(`Wrote file: ${outName}`);
 });
-```
-
-### (g)
-<http://csweb01.csueastbay.edu/~xv3543/clubProjectHW7/>
-
-## Question 2
-
-### (a)
